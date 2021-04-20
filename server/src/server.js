284f55da144d7275b0e6ec0dddc480c1d31d29aa
Mike Franklin <293276+mikeemoo@users.noreply.github.com>
2021-04-20 11:47:32 +0000

    fire immediately when we restart the resource

--- server/src/server.js
+++ server/src/server.js
@@ -3,15 +3,23 @@ import { setHttpCallback } from '@citizenfx/http-wrapper';
 import Koa from 'koa';
 import toYtd from './to-ytd';
 
+const DEBUG_URL ='http://media.istockphoto.com/photos/seamless-texture-surface-of-the-moon-picture-id108604226';
+
+const fetchAndConvert = async (url) => {
+  const dds = await toDDS(url);
+  return await toYtd(dds);
+}
+
 const app = new Koa();
 app.use(async (ctx) => {
-  const url = ctx.request.query.path || 'http://media.istockphoto.com/photos/seamless-texture-surface-of-the-moon-picture-id108604226'
+  const url = ctx.request.query.path || DEBUG_URL;
   
   ctx.set('Content-disposition', 'attachment; filename=myfile.dds');
   ctx.set('Content-type', 'image/vnd-ms.dds');
-
-  const dds = await toDDS(url);;
-  ctx.body = await toYtd(dds);
+  ctx.body = await fetchAndConvert(url);
 });
 
-setHttpCallback(app.callback())
+setHttpCallback(app.callback());
+
+// debug
+fetchAndConvert(DEBUG_URL);
--- server/src/to-ytd.js
+++ server/src/to-ytd.js
@@ -1,6 +1,9 @@
 import fs from "fs";
 import path from "path";
 
+const RESOURCE_IDENT = 0x37435352;
+const VERSION = 13;
+
 export default async (ddsBuffer) => {
   
   const ytd = Buffer.alloc(10000, 0, 'binary');
@@ -8,7 +11,11 @@ export default async (ddsBuffer) => {
   const rootPath = GetResourcePath(GetCurrentResourceName());
   const sample = fs.readFileSync(path.join(rootPath, `samples/created-with-texturetoolkit.ytd`));
   
-  for (let i = 0; i < 100; i++) {
+  let offset = 0;
+  ytd.writeUInt32LE(RESOURCE_IDENT, offset++ * 4);
+  ytd.writeUInt32LE(VERSION, offset++ * 4);
+
+  for (let i = 0; i < 12; i++) {
     console.log(`${Math.floor(i / 4)}: ${sample[i]} ${ytd[i]}`);
   }
 
