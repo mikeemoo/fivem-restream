e4af14327b69adbf356d0033ddd9a6b526132aa7
Mike Franklin <293276+mikeemoo@users.noreply.github.com>
2021-04-21 13:25:35 +0000

    got the basics working

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,3 @@
-import toDDS from './to-dds';
 import { setHttpCallback } from '@citizenfx/http-wrapper';
 import Koa from 'koa';
 import toYtd from './to-ytd';
@@ -6,21 +5,14 @@ import toYtd from './to-ytd';
 //const DEBUG_URL ='http://media.istockphoto.com/photos/seamless-texture-surface-of-the-moon-picture-id108604226';
 const DEBUG_URL = 'https://images-eu.ssl-images-amazon.com/images/I/91-kPSv2efL.png';
 
-const fetchAndConvert = async (url) => {
-  const { width, height, stride, data } = await toDDS(url);
-  return await toYtd(width, height, stride, data);
-}
-
 const app = new Koa();
 app.use(async (ctx) => {
-  const url = ctx.request.query.path || DEBUG_URL;
+  const url = (ctx.request.query.url as string) || DEBUG_URL;
+  const dictionaryName = (ctx.request.query.dictionaryName as string) || "txdictionary";
 
-  ctx.set('Content-disposition', 'attachment; filename=myfile2.dds');
-  ctx.set('Content-type', 'image/vnd-ms.dds');
-  ctx.body = await fetchAndConvert(url);
+  ctx.set('Content-disposition', `attachment; filename=${dictionaryName}.ytd`);
+  ctx.set('Content-type', 'application/x-binary');
+  ctx.body = await toYtd(url);
 });
 
 setHttpCallback(app.callback());
-
-// debug
-fetchAndConvert(DEBUG_URL);
--- src/server/to-ytd.ts
+++ src/server/to-ytd.ts
@@ -1,23 +1,18 @@
 import fs from "fs";
 import path from "path";
 import zlib from "zlib";
+import dxt from 'dxt-js';
+import pixels from 'image-pixels';
 
-const RESOURCE_IDENT = 0x37435352;
-const VERSION = 13;
-const SYSTEM_FLAGS = 131072;
-const GRAPHICS_FLAGS = 3489792006;
-
-export default async (width: number, height: number, stride: number, ddsBuffer: Buffer) => {
+export default async (url: string) => {
   
   try {
 
-    const rootPath = GetResourcePath && GetCurrentResourceName ? GetResourcePath(GetCurrentResourceName()) : __dirname;
+    const { data, width, height } = await pixels(url);
 
-    const headerBuffer = Buffer.alloc(16, 0, "binary");
-    headerBuffer.writeUInt32LE(0x37435352, 0x0000);
-    headerBuffer.writeInt32LE(13,          0x0004);
-    headerBuffer.writeUInt32LE(131072,     0x0008);
-    headerBuffer.writeUInt32LE(3489792006, 0x000C);
+    const headerBuffer = Buffer.from([
+      /*0000*/0x52, 0x53, 0x43, 0x37, 0x0D, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x04, 0x00, 0x02, 0xD0
+    ]);
 
     const contentBuffer = Buffer.from([
       /*0010*/0xD0, 0x0F, 0x57, 0x40, 0x01, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x50, 0x00, 0x00, 0x00, 0x00,
@@ -35,38 +30,48 @@ export default async (width: number, height: number, stride: number, ddsBuffer: 
       /*01E0*/0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x44, 0x58, 0x54, 0x31, 0x00, 0x01, 0x00, 0x00,
               ...Array(16).fill(0x00),
       /*0200*/0x00, 0x00, 0x00, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-              ...Array(0x1df0).fill(0x00)
+              ...Array(0x0100).fill(0x00),
+      /*0300*/0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+              ...Array(0x1CF0).fill(0x00)
     ]);
 
     const filename = "texture";
     const hashKey = GetHashKey(filename);
-    contentBuffer.writeInt32LE(hashKey,   0x0080);
-    contentBuffer.writeUInt16LE(width,    0x01D0);
-    contentBuffer.writeUInt16LE(height,   0x01D2);
-    contentBuffer.writeUInt16LE(stride,   0x01D6);
-    contentBuffer.write(filename,         0x0280);
-
-    const fullBuffer = Buffer.concat([
-      headerBuffer,
-      contentBuffer,
-      ddsBuffer
-    ]);
     
-    fs.writeFileSync(path.join(rootPath, "/samples/reconstruction.ytd"), fullBuffer);
+    contentBuffer.writeInt32LE(hashKey,     0x0080);
+    contentBuffer.writeUInt16LE(width,      0x01D0);
+    contentBuffer.writeUInt16LE(height,     0x01D2);
+    contentBuffer.writeUInt16LE(width / 2,  0x01D6);
+    contentBuffer.write(filename,           0x0280);
 
-  }catch (e) {
-    console.log(e.message, e.stack);
-  }
+    const flags = dxt.flags.DXT1 | dxt.flags.ColourIterativeClusterFit | dxt.flags.ColourMetricPerceptual;
 
-  // zlib.gzip(output, (err, result) => {
-  //   const final = Buffer.concat([
-  //     header,
-  //     result
-  //   ])
-  //   for (let i = 0; i < 100; i++) {
-  //     console.log(`${Math.floor(i / 4)}: ${final[i]} ${sample[i]}`);
-  //   }
-  // });
-  
-  return ddsBuffer;
+    const imageBuffer = dxt.compress(
+      data,
+      width,
+      height,
+      flags
+    );
+
+    return new Promise((res, rej) => {
+      
+      zlib.deflateRaw(Buffer.concat([
+        contentBuffer,
+        imageBuffer
+      ]), (err, resultBuffer) => {
+        if (err) {
+          return rej(err);
+        }
+        const finalBuffer = Buffer.concat([
+          headerBuffer,
+          resultBuffer
+        ])
+        res(finalBuffer);
+      });
+    });
+
+
+  } catch (e) {
+    throw e;
+  }  
 }
